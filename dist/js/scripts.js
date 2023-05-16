02b72c46881663efe48196de44aa8057ac11ae23
paul <paul.westenthanner@web.de>
2023-05-17 00:02:20 +0200

    added more skills

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -52,24 +52,3 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
-
-/*
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("carousel-fade");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 1000); // Change image every 1 seconds
-} 
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-*/
